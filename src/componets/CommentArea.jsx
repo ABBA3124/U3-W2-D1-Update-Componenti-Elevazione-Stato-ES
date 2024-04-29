444c3ec8a763b9f38bde55a540bf088f79a587ff
ABBA3124 <davideabbadessa73@gmail.com>
2024-04-29 19:42:08 +0200

    add loading

--- src/componets/CommentArea.jsx
+++ src/componets/CommentArea.jsx
@@ -1,19 +1,20 @@
 import React, { Component } from 'react'
 import CommentsList from './CommentsList'
 import AddComment from './AddComment'
+import Loading from './Loading'
 
 class CommentArea extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            comments: []
+            comments: [],
+            isLoading: false
         }
         this.addComment = this.addComment.bind(this)
     }
     
     componentDidMount() {
         if (this.props.book && this.props.book.asin) {
-            console.log("Props del libro cliccato:", this.props.book)
             this.fetchComments()
         }
     }
@@ -31,6 +32,7 @@ class CommentArea extends Component {
     }
   
     fetchComments = async () => {
+        this.setState({ isLoading: true })
         try {
           const response = await fetch(`https://striveschool-api.herokuapp.com/api/comments/${this.props.book.asin}`, {
             headers: {
@@ -39,18 +41,20 @@ class CommentArea extends Component {
           })
           if (response.ok) {
             const comments = await response.json()
-            console.log("Informazioni ricevute dalla api tramite id:", comments) 
-            this.setState({ comments })
+            this.setState({ comments, isLoading: false })
           } else {
             console.error('Impossibile recuperare i commenti')
+            this.setState({ isLoading: false })
           }
         } catch (error) {
           console.error('Errore durante il recupero dei commenti:', error)
+          this.setState({ isLoading: false })
         }
       }
   
 
       deleteComment = async (commentId) => {
+        this.setState({ isLoading: true })
         const response = await fetch(`https://striveschool-api.herokuapp.com/api/comments/${commentId}`, {
             method: 'DELETE',
             headers: {
@@ -60,10 +64,12 @@ class CommentArea extends Component {
 
         if (response.ok) {
             this.setState(prevState => ({
-                comments: prevState.comments.filter(comment => comment._id !== commentId)
+                comments: prevState.comments.filter(comment => comment._id !== commentId),
+                isLoading: false
             }))
         } else {
             console.error("Errore durante la cancellazione del commento")
+            this.setState({ isLoading: false })
         }
     }
 
@@ -73,8 +79,12 @@ class CommentArea extends Component {
         return (
             <div>
               <h3 className='mt-3 mb-2'>Recensioni⬇️</h3>
+              {this.state.isLoading ? <Loading /> : (
+             <>
               <CommentsList comments={this.state.comments} onDelete={this.deleteComment} />
               <AddComment book={this.props.book} onAddComment={this.addComment} />
+              </>
+              )}
           </div>
         )
       }
