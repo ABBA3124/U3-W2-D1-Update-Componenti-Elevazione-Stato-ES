3bb392fb2eb4d3140660a1c781ab09192002ef6c
ABBA3124 <davideabbadessa73@gmail.com>
2024-04-29 19:12:08 +0200

    add delete

--- src/componets/CommentArea.jsx
+++ src/componets/CommentArea.jsx
@@ -49,11 +49,31 @@ class CommentArea extends Component {
         }
       }
   
+
+      deleteComment = async (commentId) => {
+        const response = await fetch(`https://striveschool-api.herokuapp.com/api/comments/${commentId}`, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjE4ZjE0ODdmMzA0NjAwMWFlNTlmODkiLCJpYXQiOjE3MTQ0MDA2MzEsImV4cCI6MTcxNTYxMDIzMX0.rZjkpGaKszaiqZqTQJxeF8pP3knO3GoFbdgDQ6T-itk" 
+            }
+        })
+
+        if (response.ok) {
+            this.setState(prevState => ({
+                comments: prevState.comments.filter(comment => comment._id !== commentId)
+            }))
+        } else {
+            console.error("Errore durante la cancellazione del commento")
+        }
+    }
+
+
+
       render() {
         return (
             <div>
               <h3 className='mt-3 mb-2'>Recensioni⬇️</h3>
-              <CommentsList comments={this.state.comments} />
+              <CommentsList comments={this.state.comments} onDelete={this.deleteComment} />
               <AddComment book={this.props.book} onAddComment={this.addComment} />
           </div>
         )
